11e8e2c3efba972dddf25365dcb8387187f832c5
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-13 22:41:05 -0800

    Update ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,44 +1,42 @@
 
-public class ClockDriver
-{
-    public static void main(String[] args){
-        //Implement tests for your ClockDisplay
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32 to 03:33
-        //  * Tick test for 03:09 to 03:10
-        //  * Tick test for 01:59 to 02:00
-        //  * Tick test for 09:59 to 10:00
-        //  * Tick test for 23:59 to 00:00
-        
-        
-        //Implement tests for your ClockDisplaySeconds
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32:59 to 03:33:00
-        //  * Tick test for 01:00:59 to 01:01:00
-        //  * Tick test for 01:59:59 to 02:00:00
-        //  * Tick test for 23:59:59 to 00:00:00
-        
-        /*CHALLENGE*/
-        //Implement tests for your ClockDisplay12Hour
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32PM to 03:33PM
-        //  * Tick test for 11:59PM to 12:00AM
-        //  * Tick test for 11:59AM to 12:00PM
-        //  * Tick test for 12:59PM to 01:00PM
+public class ClockDriver {
+    public static void main(String[] args) {
+        // Test ClockDisplay
+        System.out.println("ClockDisplay Tests:");
+        ClockDisplay clock = new ClockDisplay();
+        System.out.println("Initial time: " + clock.readTime());
+        clock.setTime(3, 32);
+        System.out.println("Set time to 03:32: " + clock.readTime());
+        clock.tick();
+        System.out.println("After tick: " + clock.readTime());
+        clock.setTime(1, 59);
+        clock.tick();
+        System.out.println("After tick 01:59 to 02:00: " + clock.readTime());
+        clock.setTime(23, 59);
+        clock.tick();
+        System.out.println("After tick 23:59 to 00:00: " + clock.readTime());
+
+        // Test ClockDisplaySeconds
+        System.out.println("\nClockDisplaySeconds Tests:");
+        ClockDisplaySeconds clockSeconds = new ClockDisplaySeconds();
+        System.out.println("Initial time: " + clockSeconds.readTime());
+        clockSeconds.setTime(3, 32, 59);
+        System.out.println("Set time to 03:32:59: " + clockSeconds.readTime());
+        clockSeconds.tick();
+        System.out.println("After tick 03:32:59 to 03:33:00: " + clockSeconds.readTime());
+        clockSeconds.setTime(1, 59, 59);
+        clockSeconds.tick();
+        System.out.println("After tick 01:59:59 to 02:00:00: " + clockSeconds.readTime());
+
+        // Test ClockDisplay12Hour (Challenge)
+        System.out.println("\nClockDisplay12Hour Tests:");
+        ClockDisplay12Hour clock12Hour = new ClockDisplay12Hour();
+        clock12Hour.setTime(3, 32);
+        System.out.println("Set time to 03:32 AM: " + clock12Hour.readTime());
+        clock12Hour.tick();
+        System.out.println("After tick 03:32 AM to 03:33 AM: " + clock12Hour.readTime());
+        clock12Hour.setTime(11, 59);
+        clock12Hour.tick();
+        System.out.println("After tick 11:59 AM to 12:00 PM: " + clock12Hour.readTime());
     }
 }
