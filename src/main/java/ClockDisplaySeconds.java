419b4cc9ac293c19f025a05c25d9fc3b711ff6c2
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-13 22:40:31 -0800

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,32 @@
+public class ClockDisplaySeconds extends ClockDisplay {
+    private NumberDisplay seconds;
 
-public class ClockDisplaySeconds
-{
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+    public ClockDisplaySeconds() {
+        super();
+        seconds = new NumberDisplay(60); // Seconds range from 0 to 59
+    }
+
+    public ClockDisplaySeconds(int hour, int minute, int second) {
+        super(hour, minute);
+        seconds = new NumberDisplay(60);
+        setTime(hour, minute, second);
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        super.setTime(hour, minute);
+        if (second >= 0 && second < 60) {
+            seconds.setValue(second);
+        }
+    }
+
+    public String readTime() {
+        return super.readTime() + ":" + seconds.getDisplayValue();
+    }
+
+    public void tick() {
+        seconds.increment();
+        if (seconds.getValue() == 0) {
+            super.tick();
+        }
+    }
 }
