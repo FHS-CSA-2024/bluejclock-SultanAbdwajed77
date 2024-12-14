25312f38907a00da89ff87d907b1a1f87d396b88
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-13 22:40:03 -0800

    Update ClockDisplay.java

--- src/main/java/ClockDisplay.java
+++ src/main/java/ClockDisplay.java
@@ -1,40 +1,36 @@
 
-public class ClockDisplay {  
-   private NumberDisplay hours;  
-   private NumberDisplay minutes;  
-   private String displayString;  
-  
-   public ClockDisplay() {  
-      hours = new NumberDisplay(24);  
-      minutes = new NumberDisplay(60);  
-      updateDisplay();  
-   }  
-  
-   public ClockDisplay(int hour, int minute) {  
-      hours = new NumberDisplay(24);  
-      minutes = new NumberDisplay(60);  
-      setTime(hour, minute);  
-   }  
-  
-   public void timeTick() {  
-      minutes.increment();  
-      if (minutes.getValue() == 0) {  
-        hours.increment();  
-      }  
-      updateDisplay();  
-   }  
-  
-   public void setTime(int hour, int minute) {  
-      hours.setValue(hour);  
-      minutes.setValue(minute);  
-      updateDisplay();  
-   }  
-  
-   public String getTime() {  
-      return displayString;  
-   }  
-  
-   private void updateDisplay() {  
-      displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();  
-   }  
+public class ClockDisplay {
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+
+    public ClockDisplay() {
+        hours = new NumberDisplay(24); // Hours range from 0 to 23
+        minutes = new NumberDisplay(60); // Minutes range from 0 to 59
+    }
+
+    public ClockDisplay(int hour, int minute) {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        setTime(hour, minute);
+    }
+
+    public void setTime(int hour, int minute) {
+        if (hour >= 0 && hour < 24) {
+            hours.setValue(hour);
+        }
+        if (minute >= 0 && minute < 60) {
+            minutes.setValue(minute);
+        }
+    }
+
+    public String readTime() {
+        return hours.getDisplayValue() + ":" + minutes.getDisplayValue();
+    }
+
+    public void tick() {
+        minutes.increment();
+        if (minutes.getValue() == 0) {
+            hours.increment();
+        }
+    }
 }
