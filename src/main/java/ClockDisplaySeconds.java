fe3987b28062fbd037c68872ff7b64ca8eb8c1c7
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-14 22:57:34 -0800

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,32 +1,36 @@
+package src.main.java;
+
 public class ClockDisplaySeconds extends ClockDisplay {
     private NumberDisplay seconds;
 
     public ClockDisplaySeconds() {
-        super();
+        super();  // Call the ClockDisplay constructor
         seconds = new NumberDisplay(60); // Seconds range from 0 to 59
     }
 
     public ClockDisplaySeconds(int hour, int minute, int second) {
-        super(hour, minute);
+        super(hour, minute);  // Call the ClockDisplay constructor
         seconds = new NumberDisplay(60);
         setTime(hour, minute, second);
     }
 
     public void setTime(int hour, int minute, int second) {
-        super.setTime(hour, minute);
+        super.setTime(hour, minute);  // Set hours and minutes using the parent class
         if (second >= 0 && second < 60) {
             seconds.setValue(second);
         }
     }
 
+    @Override
     public String readTime() {
         return super.readTime() + ":" + seconds.getDisplayValue();
     }
 
+    @Override
     public void tick() {
         seconds.increment();
         if (seconds.getValue() == 0) {
-            super.tick();
+            super.tick();  // Call tick of parent class if seconds reset to 0
         }
     }
 }
