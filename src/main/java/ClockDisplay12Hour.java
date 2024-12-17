a8e61d699be2af46ac5ab1dc0aa0b95f1894d6b0
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-16 17:25:41 -0800

    Update ClockDisplay12Hour.java

--- src/main/java/ClockDisplay12Hour.java
+++ src/main/java/ClockDisplay12Hour.java
@@ -13,40 +13,4 @@ public class ClockDisplay12Hour {
         ampm = (hour >= 12) ? "PM" : "AM";  // Set AM/PM based on the hour
     }
 
-    @Override
-    public void setTime(int hour, int minute) {
-        super.setTime(hour, minute);
-        // Set AM/PM based on the hour
-        if (hour >= 12) {
-            ampm = "PM";
-        } else {
-            ampm = "AM";
-        }
-        if (hour > 12) {  // Adjust hour if it's greater than 12
-            hour -= 12;
-        }
-    }
-
-    @Override
-    public String readTime() {
-        // Get the time from the super class and append AM/PM
-        String time = super.readTime();
-        int hour = Integer.parseInt(time.split(":")[0]);
-        if (hour == 0) {
-            hour = 12;  // Convert 0 hour to 12 for 12-hour format
-        }
-        if (hour > 12) {
-            hour -= 12;  // Adjust hour for 12-hour format
-        }
-        return String.format("%02d:%s %s", hour, time.split(":")[1], ampm);
-    }
-
-    @Override
-    public void tick() {
-        super.tick();
-        // Change AM/PM when hour reaches 12 (12:00 to 12:01)
-        if (getHours().getValue() == 12 && getMinutes().getValue() == 0) {
-            ampm = ampm.equals("AM") ? "PM" : "AM";  // Toggle AM/PM
-        }
-    }
-}
+    
