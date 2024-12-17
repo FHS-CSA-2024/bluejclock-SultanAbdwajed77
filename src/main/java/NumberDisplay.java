eaee9cf28f7cdb1ac16f09a2fefc1227c24128a6
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-16 17:26:50 -0800

    Update NumberDisplay.java

--- src/main/java/NumberDisplay.java
+++ src/main/java/NumberDisplay.java
@@ -1,25 +1,25 @@
 package src.main.java;
 
-public class NumberDisplay {
+public class NumberDisplay{
     private int value;
     private int limit;
 
-    public NumberDisplay(int limit) {
+    public NumberDisplay(int limit){
         this.limit = limit;
         this.value = 0;
     }
 
-    public void setValue(int value) {
-        if (value >= 0 && value < limit) {
+    public void setValue(int value){
+        if (value >= 0 && value < limit){
             this.value = value;
         }
     }
 
-    public int getValue() {
+    public int getValue(){
         return value;
     }
 
-    public String getDisplayValue() {
+    public String getDisplayValue(){
         if (value < 10) {
             return "0" + value;
         } else {
@@ -27,7 +27,7 @@ public class NumberDisplay {
         }
     }
 
-    public void increment() {
+    public void increment(){
         value = (value + 1) % limit;
     }
 }
